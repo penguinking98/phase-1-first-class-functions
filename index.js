2cf792be9a66d5ff363cdf4bc150e82b24459d8c
Ben Kramer <benkramer614@gmail.com>
2021-05-20 09:22:05 -0400

    commit tree a921b03da7d9d658b198c6e847ea4d2d7ad6442b

---
+++ index.js
@@ -0,0 +1,14 @@
+function runFiveMiles() {
+    console.log('Go for a five-mile run');
+  } 
+  function liftWeights() {
+    console.log('Pump iron');
+  }
+  
+  function swimFortyLaps() {
+    console.log('Swim 40 laps');
+  } 
+  function Monday() {
+    exerciseRoutine(liftWeights);
+  } 
+  Monday()
